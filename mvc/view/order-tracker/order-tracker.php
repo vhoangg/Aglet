6393fa5617e8c77395d60b807334664ddd609dc2
vhoangg <nhoang200260@gmail.com>
2022-11-14 09:53:20 +0700

    update

--- mvc/view/order-tracker/order-tracker.php
+++ mvc/view/order-tracker/order-tracker.php
@@ -9,13 +9,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aglet Official Site</title>
     <link rel="stylesheet" href="./mvc/view/order-tracker/ordertracker.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap"
-        rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,600;0,700;0,900;1,400&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,600;0,700;0,900;1,400&display=swap" rel="stylesheet">
+    <style>
+        .header {
+            height: 165px;
+        }
+
+        .search-box .icon {
+            width: 5.125rem;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -27,8 +33,7 @@
                 <span class="form-message"></span>
             </div>
             <div class="form-group">
-                <input id="ttlienlac" name="ttlienlac" type="text" placeholder="Email/Số điện thoại"
-                    class="form-control">
+                <input id="ttlienlac" name="ttlienlac" type="text" placeholder="Email/Số điện thoại" class="form-control">
                 <span class="form-message"></span>
             </div>
             <button class="form-submit">TRA CỨU ĐƠN HÀNG</button>
