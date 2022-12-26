ae8b84c80c468e049004522130128eb3cd39f11d
Tuyết Linh <73842823+tuyetlnhh@users.noreply.github.com>
2022-12-26 15:23:14 +0700

    cart

--- mvc/view/Cart/cart.php
+++ mvc/view/Cart/cart.php
@@ -1,36 +1,34 @@
 <?php
 print_r($_SESSION['cart']);
 ?>
-
 <section id="cart" class="main-cart container-fluid">
   <div class="row">
     <div class="col col-half">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-1">GIỎ HÀNG</div>
-
+      <div class="title-1">
+        <h3>
+          GIỎ HÀNG
+        </h3>
+      </div>
       <div class="allCart">
-        <div class="product-cart">
+        <div class="product-cart main-cart-left">
           <div class="product-info">
-            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 item-2">
+            <div class="col col-big item-2">
               <div class="media">
                 <div class="media-left">
                   <a href="#">
-                    <img class="media-object" src="https://ananas.vn/wp-content/uploads/Pro_AV00008_1-500x500.jpg" width="250" height="250" alt="">
+                    <img class="media-object" src="https://ananas.vn/wp-content/uploads/Pro_AV00008_1-500x500.jpg">
                   </a>
                 </div>
                 <div class="media-body">
                   <a href="#">
                     <h4 class="media-heading">Basas bumper shoes</h4>
                   </a>
-
-                  <h5 class="price">
-                    <span class="sale-off1">
-                      <strong>Giá</strong>
-                      <p>520.000VNĐ</p>
-                    </span>
-                  </h5>
+                  <h4 class="price">
+                    Giá: 520.000VNĐ
+                  </h4>
                   <div class="row bottom">
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 sizePicker">
-                      <h5>Size</h5>
+                    <div class="sizePicker">
+                      <h4>Size</h4>
                       <select id="size-1" class="selectpicker selectSize pickSize bs-select-hidden">
                         <option>&nbsp;</option>
                         <option>35</option>
@@ -46,8 +44,8 @@ print_r($_SESSION['cart']);
                       </select>
 
                     </div>
-                    <div class=" quanPicker">
-                      <h5>Số lượng</h5>
+                    <div class="quanPicker">
+                      <h4>Số lượng</h4>
                       <select id="size-1" class="selectpicker selectQuan pickQuan bs-select-hidden">
                         <option>&nbsp;</option>
                         <option>1</option>
@@ -64,96 +62,23 @@ print_r($_SESSION['cart']);
                     </div>
                   </div>
                 </div>
-                <div class="media-right">
-                  <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 item-2-1">
-                    <div class="price">
-                      <h4>720.000VNĐ</h4>
-                    </div>
-                    <div class="status">Còn hàng</div>
-                    <div class="button">
-                      <i class="fa-solid fa-trash"></i>
-                    </div>
-                  </div>
-                </div>
+              </div>
+            </div>
+            <div class="col col-third item-2-1 media-right">
+              <div class="price orange">
+                <h4>782000000 VNĐ</h4>
+              </div>
+              <div class="status orange">Còn hàng</div>
+              <div class="button">
+                <i class="fa-solid fa-trash"></i>
               </div>
             </div>
           </div>
-
           <div class="divider-dashed"></div>
-
-          <div class="product-info">
-            <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 item-2">
-              <div class="media">
-                <div class="media-left">
-                  <a href="#">
-                    <img class="media-object" src="https://ananas.vn/wp-content/uploads/Pro_AV00008_1-500x500.jpg" width="250" height="250" alt="">
-                  </a>
-                </div>
-                <div class="media-body">
-                  <a href="#">
-                    <h4 class="media-heading">Basas bumper shoes</h4>
-                  </a>
-
-                  <h5 class="price">
-                    <span class="sale-off1">
-                      <strong>Giá</strong>
-                      <p>520.000VNĐ</p>
-                    </span>
-                  </h5>
-                  <div class="row bottom">
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 sizePicker">
-                      <h5>Size</h5>
-                      <select id="size-1" class="selectpicker selectSize pickSize bs-select-hidden">
-                        <option>&nbsp;</option>
-                        <option>35</option>
-                        <option>36</option>
-                        <option>37</option>
-                        <option>38</option>
-                        <option>39</option>
-                        <option>40</option>
-                        <option>41</option>
-                        <option>42</option>
-                        <option>43</option>
-                        <option>44</option>
-                      </select>
-
-                    </div>
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 quanPicker">
-                      <h5>Số lượng</h5>
-                      <select id="size-1" class="selectpicker selectQuan pickQuan bs-select-hidden">
-                        <option>&nbsp;</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="media-right">
-                  <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 item-2-1">
-                    <div class="price">
-                      <h4>720.000VNĐ</h4>
-                    </div>
-                    <div class="status">Còn hàng</div>
-                    <div class="button">
-                      <i class="fa-solid fa-trash"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
-    <div class="col col-third">
+    <div class="col-third ">
       <ul class="bill">
         <li class="group_title">ĐƠN HÀNG</li>
         <li class="divider"></li>
