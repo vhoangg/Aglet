80cfb0c0ba6b383efcf4f4969793e1e70bf21540
Tuyết Linh <73842823+tuyetlnhh@users.noreply.github.com>
2022-12-28 01:31:51 +0700

    order detail

--- mvc/view/Admin/orderManagement/orderManagement.php
+++ mvc/view/Admin/orderManagement/orderManagement.php
@@ -1,10 +1,9 @@
-
 <div class="input-group d-flex flex-row justify-content-center mt-3 mb-3">
-    <div class="col-md-4">
-        <input type="search" class="form-control  rounded " placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  <div class="col-md-4">
+    <input type="search" class="form-control  rounded " placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
 
-    </div>
-    <button type="button" class="btn btn-outline-primary">search</button>
+  </div>
+  <button type="button" class="btn btn-outline-primary">search</button>
 </div>
 
 <table class="table table-hover">
@@ -19,88 +18,88 @@
     </tr>
   </thead>
   <tbody>
-		<?php
-			$i = 0;
-			$row = [];
-			while($row[$i] = mysqli_fetch_array($data["inv"])){
-        $str='';
-        switch ($row[$i]['status']) {
-          case 0:
-            $str = "Chưa xác nhận";
-            break;
-          case 1:
-            $str = "Đã chuyển giao";
-            break;
-          case 2:
-            $str = "Đang giao";
-            break;
-          case 3:
-            $str = "Đã nhận hàng";
-            break;
+    <?php
+    $i = 0;
+    $row = [];
+    while ($row[$i] = mysqli_fetch_array($data["inv"])) {
+      $str = '';
+      switch ($row[$i]['status']) {
+        case 0:
+          $str = "Chưa xác nhận";
+          break;
+        case 1:
+          $str = "Đã chuyển giao";
+          break;
+        case 2:
+          $str = "Đang giao";
+          break;
+        case 3:
+          $str = "Đã nhận hàng";
+          break;
 
-          default:
-            $str = "Đã hủy";
-            break;
-        }
-				echo '
+        default:
+          $str = "Đã hủy";
+          break;
+      }
+      echo '
 				<tr>
-				<td>'.$row[$i]['id'].'</td>
-				<td>'.$row[$i]['phone'].'</td>
-				<td>'.$row[$i]['email'].'</td>
-				<td>'.$row[$i]['created_at'].'</td>
-        <td>'.$str.'</td>
+				<td>' . $row[$i]['id'] . '</td>
+				<td>' . $row[$i]['phone'] . '</td>
+				<td>' . $row[$i]['email'] . '</td>
+				<td>' . $row[$i]['created_at'] . '</td>
+        <td>' . $str . '</td>
         <td>
         <div class="d-flex flex-row justify-content-around">
-          <a href = "http://localhost/aglet/admin/orderDetail?id='.$row[$i]["id"].'"><i class="fa-regular fa-circle-info"></i></a></li>
+          <a href = "http://localhost/aglet/admin/orderDetail?id=' . $row[$i]["id"] . '"><i class="fa-regular fa-circle-info"></i></a></li>
 
     </div>
         </td>
 				';
-				$i++;
-			}
-		?>
+      $i++;
+    }
+    ?>
   </tbody>
 </table>
 
 
 <div class="demo">
-    <nav class="pagination-outer" aria-label="Page navigation">
-        <ul class="pagination">
-            <li class="page-item">
-                <a href="<?php
-                $page = $data["pg"];
-                if($page[0] == 1)
-                    echo "http://localhost/aglet/admin/productManagement/".($page[0]);
-                else
-                    echo "http://localhost/aglet/admin/productManagement/".($page[0]-1);
-                ?>" class="page-link" aria-label="Previous">
-                    <span aria-hidden="true">«</span>
-                </a>
-            </li>
-            <?php
+  <nav class="pagination-outer" aria-label="Page navigation">
+    <ul class="pagination">
+      <li class="page-item">
+        <a href="<?php
+                  $page = $data["pg"];
+                  if ($page[0] == 1)
+                    echo "http://localhost/aglet/admin/orderManagement/" . ($page[0]);
+                  else
+                    echo "http://localhost/aglet/admin/orderManagement/" . ($page[0] - 1);
+                  ?>" class="page-link" aria-label="Previous">
+          <span aria-hidden="true">«</span>
+        </a>
+      </li>
+      <?php
 
-                $num = mysqli_fetch_array($data["num"]);
-                $per_page = 10;
-                $pages = ceil($num[0] / $per_page);
-                $i = 1;
-                while($i <= $pages){
-                    echo "
-                        <li class=","page-item","><a class=","page-link"," href= ","http://localhost/aglet/admin/productManagement/".$i.">".$i."</a></li>
+      $num = mysqli_fetch_array($data["num"]);
+      $per_page = 10;
+      $pages = ceil($num[0] / $per_page);
+      $i = 1;
+      while ($i <= $pages) {
+        echo "
+                        <li class=", "page-item", "><a class=", "page-link", " href= ", "http://localhost/aglet/admin/orderManagement/" . $i . ">" . $i . "</a></li>
                     ";
-                    $i++;
-                }
+        $i++;
+      }
 
 
-            ?>
+      ?>
 
-            <li class="page-item">
-                <a href="<?php
-                $page = $data["pg"];
-                echo "http://localhost/aglet/admin/productManagement/".($page[0]+1);
-                ?> " class="page-link" aria-label="Next">
-                    <span aria-hidden="true">»</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+      <li class="page-item">
+        <a href="<?php
+                  $page = $data["pg"];
+                  echo "http://localhost/aglet/admin/orderManagement/" . ($page[0] + 1);
+                  ?> " class="page-link" aria-label="Next">
+          <span aria-hidden="true">»</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </div>
